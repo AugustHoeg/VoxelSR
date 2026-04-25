37d966c8f7a4e7d9064198229e9e0df28639c8fc
AugustHoeg <aulho@dtu.dk>
2026-04-25 16:30:13 +0200

    'update'

--- upload_huggingface.py
+++ upload_huggingface.py
@@ -7,8 +7,8 @@ api = HfApi()
 dataset_path = "/dtu/3d-imaging-center/projects/2025_DANFIX_163_VoDaSuRe/analysis/VoDaSuRe_dataset_p4/VoDaSuRe/"
 
 api.upload_file(
-    path_or_fileobj=os.path.join(dataset_path, "README.md"),
-    path_in_repo="README.md",
+    path_or_fileobj=os.path.join(dataset_path, "extract_files.sh"),
+    path_in_repo="extract_files.sh",
     repo_id="AugustHoeg/VoDaSuRe",
     repo_type="dataset",
 )
